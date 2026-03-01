c049e74b7a3501bd7c90c538199d511ef8d8f611
AC-Baleni <baleniicaviwe@gmail.com>
2026-03-01 21:52:48 +0200

    Update: Did some background styling on the landing page

--- js/script.js
+++ js/script.js
@@ -5,3 +5,28 @@ const nav = document.querySelector(".nav-links");
 toggle.addEventListener("click", () => {
   nav.style.display = nav.style.display === "flex" ? "none" : "flex";
 });
+
+const rainContainer = document.querySelector(".rain");
+
+function createRain() {
+  for (let i = 0; i < 120; i++) {
+    const drop = document.createElement("div");
+    drop.classList.add("drop");
+
+    // random horizontal position
+    drop.style.left = Math.random() * 100 + "vw";
+
+    // random speed
+    drop.style.animationDuration = Math.random() * 4 + 3.5 + "s";
+
+    // random delay
+    drop.style.animationDelay = Math.random() * 2 + "s";
+
+    // random size
+    drop.style.height = Math.random() * 10 + 10 + "px";
+
+    rainContainer.appendChild(drop);
+  }
+}
+
+createRain();
